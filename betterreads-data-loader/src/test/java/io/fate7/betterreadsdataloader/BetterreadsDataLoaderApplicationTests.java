8727641164de7378e989c5e191f3273234492848
fate7 <boucenna.fateh@gmail.com>
2021-10-21 12:18:59 +0100

    Init

---
+++ betterreads-data-loader/src/test/java/io/fate7/betterreadsdataloader/BetterreadsDataLoaderApplicationTests.java
@@ -0,0 +1,13 @@
+package io.fate7.betterreadsdataloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetterreadsDataLoaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
